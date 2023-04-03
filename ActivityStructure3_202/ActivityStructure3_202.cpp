2bbf775088e5a15367c5f740c37f4bfca709aec9
wawanpi <muhaguskurniawan@gmail.com>
2023-04-03 15:29:02 +0700

    pengulangan dan input

--- ActivityStructure3_202/ActivityStructure3_202.cpp
+++ ActivityStructure3_202/ActivityStructure3_202.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+using namespace std;
 
 struct AlamatDetail
 {
@@ -14,3 +14,24 @@ struct Mahasiswa
 	AlamatDetail alamat;
 	int umur;
 };
+
+int main() {
+	Mahasiswa mhs[3];
+
+	for (int i = 0; 1 < 3; i++)
+	{
+		cout << "Masukkan nim: ";
+		cin.getline(mhs[i].nim, 12);
+		cout << "Masukkan nama: ";
+		cin.getline(mhs[i].nama, 20);
+		cout << "Alamat: " << endl;
+		cout << "\tNama desa : ";
+		cin.getline(mhs[i].alamat.desa, 20);
+		cout << "\tNama kota : ";
+		cin.getline(mhs[i].alamat.kota, 20);
+		cout << "Masukkan umur: ";
+		cin >> mhs[i].umur;
+		cin.ignore(1, '\n');
+	}
+
+}
